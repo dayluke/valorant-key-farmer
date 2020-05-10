5a094704e7c4e1263183488e8677d15d3c5a950f
dayluke <dayluke01@gmail.com>
2020-05-10 16:47:08 +0100

    Moved code into functions and added some general comments

--- valorant-key.py
+++ valorant-key.py
@@ -1,7 +1,38 @@
 import time
 from selenium import webdriver
 
+# initialise the selenium webdriver
 driver = webdriver.Chrome()
-driver.get('https://www.twitch.tv/directory/all/tags/c2542d6d-cd10-4532-919b-3d19f30a768b');
-time.sleep(5)
-first_twitch_stream = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/main/div[1]/div[3]/div/div/div/div[4]/div/div[1]/div[2]/div/div/div/div/article/div[1]/div/div/div[1]/div/a").click()
+
+def get_element(xpath):
+     # wait for the previous command to be executed (e.g. page load)
+    time.sleep(5)
+    return driver.find_element_by_xpath(xpath)
+
+
+def load_stream():
+    first_twitch_stream = get_element("/html/body/div[1]/div/div[2]/div[2]/main/div[1]/div[3]/div/div/div/div[4]/div/div[1]/div[2]/div/div/div/div/article/div[1]/div/div/div[1]/div/a")
+    print("twitch stream successfully found")
+    first_twitch_stream.click()
+    print("twitch stream successfully loaded")
+
+
+def main():
+    # load the current twitch live streams that have the tag 'drops enabled'
+    driver.get('https://www.twitch.tv/directory/all/tags/c2542d6d-cd10-4532-919b-3d19f30a768b')
+    print("twitch loaded correctly")
+
+    load_stream()
+
+    video_player = get_element("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div/div/div[5]")
+    print("video player element successfully found")
+
+    if not video_player.get_attribute("class") == "tw-c-text-overlay":
+        print("stream is no longer live")
+        print(video_player.get_attribute("class"))
+        print("loading new stream...")
+    else:
+        print("stream is live")
+
+if __name__ == "__main__": 
+    main()
