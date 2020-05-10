221f30c046c6047fdc80f093eb4f493e8615ed01
dayluke <dayluke01@gmail.com>
2020-05-10 16:59:03 +0100

    Added an is_stream_live method

--- valorant-key.py
+++ valorant-key.py
@@ -1,6 +1,11 @@
 import time
 from selenium import webdriver
 
+time_out_delay = 5
+
+# every x minutes, we will check if the stream is still live
+live_time_checker = 10 * 60
+
 # initialise the selenium webdriver
 driver = webdriver.Chrome()
 
@@ -12,7 +17,7 @@ def load_twitch():
 
 def get_element(xpath):
      # wait for the previous command to be executed (e.g. page load)
-    time.sleep(5)
+    time.sleep(time_out_delay)
     return driver.find_element_by_xpath(xpath)
 
 
@@ -23,21 +28,27 @@ def load_stream():
     print("twitch stream successfully loaded")
 
 
-def main():
-    load_twitch()
-    load_stream()
-
+def is_stream_live():
     video_player = get_element("/html/body/div[1]/div/div[2]/div[2]/main/div[2]/div[3]/div/div/div[2]/div/div[2]/div/div/div/div[5]")
     print("video player element successfully found")
 
-    if not video_player.get_attribute("class") == "tw-c-text-overlay":
-        print("stream is no longer live")
-        print(video_player.get_attribute("class"))
-        print("loading new stream...")
-        load_twitch()
-        load_stream()
-    else:
-        print("stream is live")
+    return video_player.get_attribute("class") == "tw-c-text-overlay"
+
+def main():
+    load_twitch()
+    load_stream()
+    
+    while True:
+        if not is_stream_live():
+            print("stream is no longer live")
+            print(video_player.get_attribute("class"))
+            print("loading new stream...")
+            load_twitch()
+            load_stream()
+        else:
+            print("stream is still live")
+            time.sleep(live_time_checker)
+
 
 if __name__ == "__main__": 
     main()
